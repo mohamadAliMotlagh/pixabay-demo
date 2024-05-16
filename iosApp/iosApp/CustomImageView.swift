ef51b00be32cc4672b824543446e949a31263850
Seyed Mojtaba Hosseini Zeidabadi <Chenzook+GitHub@gmail.com>
2024-05-16 12:27:37 +0330

    refactor: remove unnecessary VStack wrapper inside the CustomImageView

--- iosApp/iosApp/CustomImageView.swift
+++ iosApp/iosApp/CustomImageView.swift
@@ -5,12 +5,10 @@ import SDWebImageSwiftUI
 struct CustomImageView: View {
     let imageModel: SearchResultDomainModel
     var body: some View {
-        VStack {
-            WebImage(url: URL(string: imageModel.thumbnail))
-                .resizable()
-                .aspectRatio(CGFloat(imageModel.ratio), contentMode: .fit)
-                .clipped()
-                .cornerRadius(16)
-        }
+        WebImage(url: URL(string: imageModel.thumbnail))
+            .resizable()
+            .aspectRatio(CGFloat(imageModel.ratio), contentMode: .fit)
+            .clipped()
+            .cornerRadius(16)
     }
 }
