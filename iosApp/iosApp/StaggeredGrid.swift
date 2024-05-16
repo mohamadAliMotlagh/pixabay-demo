9ab2c7da713de3b1e67554f13f44dd1329cc9c0c
Seyed Mojtaba Hosseini Zeidabadi <Chenzook+GitHub@gmail.com>
2024-05-16 12:37:57 +0330

    chore: cleanup formatting in the StaggeredGrid file
    refactor: use trailing closures in a more swifty way

--- iosApp/iosApp/StaggeredGrid.swift
+++ iosApp/iosApp/StaggeredGrid.swift
@@ -13,30 +13,28 @@ import SwiftUI
 struct StaggeredGrid: View {
     @StateObject var viewModel = SearchViewModel()
 
-    
     private let columns: [GridItem] = Array(repeating: GridItem(.flexible()), count: 2)
     
     let spacing: CGFloat = 12
-    var body: some View{
+
+    var body: some View {
         NavigationView {
-            ScrollView{
-                HStack(alignment: .top,spacing: spacing) {
-                    LazyVStack(content: {
-                        ForEach(viewModel.searchResults1, id: \.id) {model in
+            ScrollView {
+                HStack(alignment: .top, spacing: spacing) {
+                    LazyVStack {
+                        ForEach(viewModel.searchResults1, id: \.id) { model in
                             CustomImageView(imageModel: model)
                         }
-                    })
-                    LazyVStack(content: {
-                        ForEach(viewModel.searchResults2, id: \.id) {model in
+                    }
+                    LazyVStack {
+                        ForEach(viewModel.searchResults2, id: \.id) { model in
                             CustomImageView(imageModel: model)
                         }
-                    })
-                    
+                    }
                 }.padding(10)
             }.navigationTitle("Pixabay Photos")
-        } .searchable(text: $viewModel.value)
+        }.searchable(text: $viewModel.value)
     }
-    
 }
 
 struct StaggeredGrid_Preview: PreviewProvider {
