bb1c93bce2053ab74f41d4f56007909f3623a4ce
Seyed Mojtaba Hosseini Zeidabadi <Chenzook+GitHub@gmail.com>
2024-05-16 12:24:46 +0330

    chore: reindent the CustomImageView file
    chore: remove extra spaces in the CustomImageView file
    chore: remove duplicate import in the CustomImageView file

--- iosApp/iosApp/CustomImageView.swift
+++ iosApp/iosApp/CustomImageView.swift
@@ -1,18 +1,16 @@
 import SwiftUI
 import shared
 import SDWebImageSwiftUI
-import SwiftUI
-    
+
 struct CustomImageView: View {
     let imageModel: SearchResultDomainModel
     var body: some View {
         VStack {
-                       WebImage(url: URL(string: imageModel.thumbnail))
-                           .resizable()
-                           .aspectRatio(CGFloat(imageModel.ratio), contentMode: .fit)
-                           .clipped()
-                           .cornerRadius(16)
-                   
-                   }
+            WebImage(url: URL(string: imageModel.thumbnail))
+                .resizable()
+                .aspectRatio(CGFloat(imageModel.ratio), contentMode: .fit)
+                .clipped()
+                .cornerRadius(16)
+        }
     }
 }
