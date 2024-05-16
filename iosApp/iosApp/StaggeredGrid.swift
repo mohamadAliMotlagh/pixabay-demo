e8d92692b9c931b992645e8553b4fb0009327f73
Seyed Mojtaba Hosseini Zeidabadi <Chenzook+GitHub@gmail.com>
2024-05-16 19:41:08 +0330

    refactor: reduce the calculations and use the original search results

--- iosApp/iosApp/StaggeredGrid.swift
+++ iosApp/iosApp/StaggeredGrid.swift
@@ -34,7 +34,3 @@ struct StaggeredGrid_Preview: PreviewProvider {
         StaggeredGrid()
     }
 }
-
-extension SearchViewModel {
-    var searchResults: [SearchResultDomainModel] { searchResults1 + searchResults2 }
-}
